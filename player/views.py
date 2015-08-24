eb7f997b4f7332b51960b039e4723418937af7b4
Michał Borgieł <michal.borgiel@gmail.com>
2015-08-24 14:19:04 +0200

    wybor wroga i shop

--- player/views.py
+++ player/views.py
@@ -9,4 +9,6 @@ def index(request, p):
         'p': gamer,
         'armor': ArmorItem.objects.get(name=gamer.armorid).value,
         'pay': int(gold_to_pay),
+        'health': gamer.health * 100 / gamer.maxhealth,
+        'mana': gamer.mana * 100 / gamer.maxmana,
     })
